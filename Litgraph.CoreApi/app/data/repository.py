61889575bd630178f04e0a635e150707a528df55
PacificNorthwest <crimea165@gmail.com>
2019-05-04 19:35:02 +0300

    Basic Litgraph GraphQL scheme added

--- Litgraph.CoreApi/app/data/repository.py
+++ Litgraph.CoreApi/app/data/repository.py
@@ -1,4 +1,44 @@
-class User:
+class Character:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+
+class Location:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Material:
+    def __init__(self, id, title, characters, locations):
+        self.id = id
+        self.title = title
+        self.characters = characters
+        self.locations = locations
+
+class User:
+    def __init__(self, id, username, email):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.materials = [
+            Material(0, "Harry Potter", [
+                Character(0, "Harry Potter"),
+                Character(1, "Hermione Granger")
+            ], 
+            [
+                Location(0, "Hogwarts"),
+                Location(1, "London")
+            ]),
+            Material(1, "Lord of the rings", [
+                Character(2, "Frodo Baggins"),
+                Character(3, "Sauron")
+            ],
+            [
+                Location(2, "Shire"),
+                Location(3, "Minas Tirit")
+            ])
+        ]
+
+def get_user(email):
+    return User(0, 'PacificNorthwest', email)
+
--- Litgraph.CoreApi/app/schema.py
+++ Litgraph.CoreApi/app/schema.py
@@ -1,14 +1,31 @@
 import graphene
 from app.data import repository
 
-class UserSchema(graphene.ObjectType):
-    name = graphene.String()
+class CharacterScheme(graphene.ObjectType):
     id = graphene.String()
+    name = graphene.String()
+
+class LocationScheme(graphene.ObjectType):
+    id = graphene.String()
+    name = graphene.String()
+
+class MaterialSchema(graphene.ObjectType):
+    id = graphene.String()
+    title = graphene.String()
+
+    characters = graphene.List(CharacterScheme)
+    locations = graphene.List(LocationScheme)
+
+class UserSchema(graphene.ObjectType):
+    id = graphene.String()
+    username = graphene.String()
+    email = graphene.String()
+    materials = graphene.List(MaterialSchema)
 
 class Query(graphene.ObjectType):
-    user = graphene.Field(UserSchema, name=graphene.String())
+    user = graphene.Field(UserSchema, email=graphene.String())
 
-    def resolve_user(self, info, name):
-        return repository.User(0, name)
+    def resolve_user(self, info, email):
+        return repository.get_user(email)
 
 schema = graphene.Schema(query=Query)
